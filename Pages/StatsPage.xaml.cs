17b1a61d96cae489962e2f10244d59ae6628347f
Alex Bolgan <olex.bolgan@gmail.com>
2024-12-02 17:42:22 +0200

    feat: add RecalledChart so that a user has 2 charts displaying added and recalled words number

--- Pages/StatsPage.xaml.cs
+++ Pages/StatsPage.xaml.cs
@@ -11,26 +11,41 @@ public partial class StatsPage : ContentPage
 	public StatsPage()
 	{
 		InitializeComponent();
-        SKColor color = SKColor.Parse("#266489");
+        var addedNumberColor = SKColor.Parse("#FF6347");
+        var uniqueRecalledNumberColor = SKColor.Parse("#4169E1");
+
         var addedNumbersEntries = context.StatsRecords.Select(stat => new Microcharts.ChartEntry(stat.AddedNumber)
         {
             Label = stat.Date.ToString("MM/dd/yyyy"),
             ValueLabel = stat.AddedNumber.ToString(),
-            Color = color
+            Color = addedNumberColor
         }).ToList();
 
         var recalledNumberEntries = context.StatsRecords.Select(stat => new Microcharts.ChartEntry(stat.UniqueRecalledNumber)
         {
             Label = stat.Date.ToString("MM/dd/yyyy"),
             ValueLabel = stat.UniqueRecalledNumber.ToString(),
-            Color = color
+            Color = uniqueRecalledNumberColor
         }).ToList();
 
         if (addedNumbersEntries.Count > 0 && recalledNumberEntries.Count > 0)
         {
-            ChartView.Chart = new LineChart()
+            AddedChart.Chart = new LineChart()
             {
-                Entries = addedNumbersEntries.Concat(recalledNumberEntries)
+                Entries = addedNumbersEntries.Concat(recalledNumberEntries),
+                LineMode = LineMode.Straight,
+                LineSize = 4,
+                PointMode = PointMode.Circle,
+                PointSize = 8
+            };
+
+            RecalledChart.Chart = new LineChart()
+            {
+                Entries = addedNumbersEntries.Concat(recalledNumberEntries),
+                LineMode = LineMode.Straight,
+                LineSize = 4,
+                PointMode = PointMode.Circle,
+                PointSize = 8
             };
         }
         else
